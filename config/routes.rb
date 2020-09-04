2d0806a8be450d24cc5f8dbed8727127f3fbc1b7
github username <github email address>
2020-09-03 23:20:40 -0400

    Add class method from_omniauth and take in authorization in user model, update and add additional  devise routes in routes.rb for omniauth functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, :controllers => {registrations: 'registrations' }
+  devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
 
   get '/' => 'sessions#index'
   get '/login' => 'sessions#new'
