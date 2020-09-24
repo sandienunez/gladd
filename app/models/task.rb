f19adf774a03d91d049fbdca3aa71e128b973953
github username <github email address>
2020-09-23 23:27:08 -0400

    Update custom validation to limit number of tasks for each user in task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,28 +6,13 @@ class Task < ApplicationRecord
    belongs_to :user, optional: true  
    has_many :comments, dependent: :destroy
    has_many :users, through: :comments #ppl who have commented on it #joint table is comments
-
    validates :task_name, :action_one, :action_two, :action_three, :date, :priority_ranking, presence: true 
-   validate :limit_number_of_tasks
+   validate :limit_number_of_tasks, on: :create
 
    def limit_number_of_tasks
-      today_tasks = user.tasks.select do |t|
-         t.created_at.try(:to_date) == Date.today
-      end
-
-      if today_tasks.size > 6
-         errors.add(:task_id, "error: In order to ensure that you focus on finishing your tasks, you can't add more then 6 tasks per day.")
-      end 
+      # binding.pry
+      if user.tasks.size > 5 
+         errors.add(:base, "Error: To help you focus on finishing your tasks, you cannot add more then 6 tasks per day.")
+       end
    end
 end
-
-         #try method tries to call to_date method and if it causese error return value is nil
-    #binding.pry 
-      #inside of here we have access to instance method and we can see self
-      #check if: are there already 2 tasks added to the db with created_at as current date
-      #iterate over user's tasks
-
-   #if we dont meet requirements #check for negative 
-      #add error for that attribute
-      #else
-      #we do nothing 
